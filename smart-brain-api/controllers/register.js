f6f6efcfdf5ddf6b0d49594a0a99550b29e68af7
Pedro Kozilek <pedro.kozilek@meseems.com.br>
2020-04-29 08:58:57 -0300

    security review

--- smart-brain-api/controllers/register.js
+++ smart-brain-api/controllers/register.js
@@ -1,5 +1,8 @@
 const handleRegister = (db, bcrypt) => (req, res) => {
     const {email, name, password} = req.body;
+    if (!email || !name || !password){
+        return res.status(400).json('incorrect form submission')
+    }
     bcrypt.hash(password, null, null, function(err, hash) {});
     const hash = bcrypt.hashSync(password);
         db.transaction(trx => {
