db4fa64d7b30205df5f2cbd977e26b68fd976a09
Pedro Kozilek <pedro.kozilek@meseems.com.br>
2020-04-29 19:55:49 -0300

    Fix typo on url fetch

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3001/image', {
+        fetch('https://polar-temple-80828.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
